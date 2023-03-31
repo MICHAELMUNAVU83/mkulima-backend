9506a9ea9ef5e6acf6929153340876937368de97
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-03-31 15:35:41 +0300

    Setup

--- app/models/county.rb
+++ app/models/county.rb
@@ -1,5 +1,5 @@
 class County < ApplicationRecord
     has_many :years
     has_many :constituencies
-   has_many :plantable_crops
+  
 end
--- app/serializers/county_serializer.rb
+++ app/serializers/county_serializer.rb
@@ -1,6 +1,7 @@
 class CountySerializer < ActiveModel::Serializer
     attributes :id, :name , :latitude, :longitude
     has_many :constituencies
-    has_many :crops
+  
+    
   end
   
